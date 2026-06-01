704f52a98a1c51e3f260468a7617b56bb76d2fb8
dufontdd <dufont.dd@gmail.com>
2026-06-01 15:36:34 +0200

    feat: add delet file

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * delete_dnodeint_at_index - Supprime le nœud à un index donné
- * @head: Double pointeur vers la tête de la liste
- * @index: Index du nœud à supprimer (commence à 0)
- *
- * Return: 1 si succès, -1 si échec
- */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current;
@@ -18,7 +11,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
     current = *head;
 
-    # 1. Si on doit supprimer le tout premier nœud (index 0)
     if (index == 0)
     {
         *head = current->next;
@@ -28,24 +20,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         return (1);
     }
 
-    # 2. On avance jusqu'au nœud à l'index demandé
     for (i = 0; current != NULL && i < index; i++)
     {
         current = current->next;
     }
 
-    # Si l'index est hors des limites de la liste
     if (current == NULL)
         return (-1);
 
-    # 3. On ajuste les pointeurs pour contourner le nœud 'current'
     if (current->prev != NULL)
         current->prev->next = current->next;
 
     if (current->next != NULL)
         current->next->prev = current->prev;
 
-    # 4. On libère la mémoire du nœud supprimé
     free(current);
     return (1);
 }
