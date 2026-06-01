f87f961dbefc125d165b4953ed17c6cc256c8153
dufontdd <dufont.dd@gmail.com>
2026-06-01 15:23:04 +0200

    feat: fix task0

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """FizzBuzz"""
+import sys  # <--- Étape 1 : On importe sys pour lire l'argument
 
 
-def fizzbuzz():
-    """Print numbers from 1 to 100 following FizzBuzz rules"""
-    for i in range(1, 101):
+def fizzbuzz(n):
+    """Print numbers from 1 to n following FizzBuzz rules"""
+    # Étape 2 : On va jusqu'à n + 1 pour inclure le nombre n
+    for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -13,3 +15,11 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+
+
+if __name__ == '__main__':
+    # On vérifie qu'un argument a bien été fourni
+    if len(sys.argv) > 1:
+        # On convertit l'argument string en entier
+        upper_limit = int(sys.argv[1])
+        fizzbuzz(upper_limit)
