a9c73c301979f3dab6ea9e579838c6556231913e
dufontdd <dufont.dd@gmail.com>
2026-06-01 11:20:52 +0200

    feat: add 0

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""FizzBuzz"""
+
+
+def fizzbuzz():
+    """Print numbers from 1 to 100 following FizzBuzz rules"""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
